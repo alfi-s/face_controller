a2f73763b7f072b4bf9c1e724248b8c5a64fb4f5
alfi-s <alfisumadi@gmail.com>
2018-12-01 18:52:09 +0000

    Keyboard input

--- detection.py
+++ detection.py
@@ -1,43 +1,79 @@
+from pynput.keyboard import Key, Controller
 import numpy as np 
 import cv2
 
+# Thresholds before sensing inputs
+x_threshold = 50
+y_threshold = 40
+
+# The keys to press
+key_x_below_threshold = 'a' # x < x_threshold
+key_x_above_threshold = 'b' # x > x_threshold
+key_y_below_threshold = 'c' # y < y_threshold
+key_y_above_threshold = 'd' # y > y_threshold
+
+# Converts screen coordinates to cartesian coordinates
 def cartesian_coordinates(sx, sy, fw, fh):
     cx = sx - (fw/2)
     cy = (fh/2) - sy
     return (cx, cy)
 
+# Gets the midpoint of a box
 def middle_of_box (x,y,w,h):
     mx = x + (w/2)
     my = y + (h/2)
     return mx, my
 
-face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
+def main():
+    # Keyboard Controller for inputs
+    keyboard = Controller()
 
-cap = cv2.VideoCapture(0)
+    # Haar Cascade for face detction
+    face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
 
-while(True):
-    ret, frame = cap.read()
+    # The webcam capture
+    cap = cv2.VideoCapture(0)
     f_width = cap.get(3)
     f_height = cap.get(4)
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    while(True):
+        ret, frame = cap.read()
 
-    for (x,y,w,h) in faces:
-        mx, my = middle_of_box(x,y,w,h)
-        cx, cy = cartesian_coordinates(mx,my,f_width,f_height)
-        print(cx, cy)
+        # Detect the faces
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
 
-        rect_color = (255,0,0)
-        stroke = 2
-        end_x = x + w
-        end_y = y + h
-        cv2.rectangle(frame, (x,y), (end_x,end_y), rect_color, stroke)
+        for (x,y,w,h) in faces:
+            # Convert to cartesian
+            mx, my = middle_of_box(x,y,w,h)
+            cx, cy = cartesian_coordinates(mx,my,f_width,f_height)
+            print(cx, cy)
+            
+            # Control the inputs based on the position of the face
+            if (cx < -x_threshold):
+                keyboard.press(key_x_below_threshold)
+            elif (cx > x_threshold):
+                keyboard.press(key_x_above_threshold)
+            if (cy < -y_threshold):
+                keyboard.press(key_y_below_threshold)
+            elif (cy > y_threshold):
+                keyboard.press(key_y_above_threshold)
+
+            # Draw a rectangle at the face
+            rect_color = (255,0,0)
+            stroke = 2
+            end_x = x + w
+            end_y = y + h
+            cv2.rectangle(frame, (x,y), (end_x,end_y), rect_color, stroke)
+        
+        # The web cam frame
+        cv2.imshow('WebCam',frame)
+        if cv2.waitKey(20) & 0xFF == ord('q'):
+            break
+
+    # When done, exit cleanly
+    cap.release()
+    cv2.destroyAllWindows()
     
-
-    cv2.imshow('WebCam',frame)
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    main()
