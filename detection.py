793d035c36f6ea5ff880fa23f0650ebcb38919e3
alfi-s <alfisumadi@gmail.com>
2018-12-01 19:19:14 +0000

    formatting

--- detection.py
+++ detection.py
@@ -2,15 +2,23 @@ from pynput.keyboard import Key, Controller
 import numpy as np 
 import cv2
 
+#######################
+### SETUP CONSTANTS ###
+#######################
+
 # Thresholds before sensing inputs
 x_threshold = 50
-y_threshold = 40
+y_threshold = 50
 
 # The keys to press
-key_x_below_threshold = 'a' # x < x_threshold
-key_x_above_threshold = 'b' # x > x_threshold
-key_y_below_threshold = 'c' # y < y_threshold
-key_y_above_threshold = 'd' # y > y_threshold
+key_x_below_threshold = Key.right    # x < x_threshold
+key_x_above_threshold = Key.left   # x > x_threshold
+key_y_below_threshold = Key.down    # y < y_threshold
+key_y_above_threshold = Key.up      # y > y_threshold
+
+############################
+### FUNCTION DEFINITIONS ###
+############################
 
 # Converts screen coordinates to cartesian coordinates
 def cartesian_coordinates(sx, sy, fw, fh):
@@ -24,7 +32,13 @@ def middle_of_box (x,y,w,h):
     my = y + (h/2)
     return mx, my
 
+########################
+### MAIN CODE MODULE ###
+########################
+
 def main():
+    wait_for_inputs = 100
+
     # Keyboard Controller for inputs
     keyboard = Controller()
 
@@ -37,6 +51,7 @@ def main():
     f_height = cap.get(4)
 
     while(True):
+
         ret, frame = cap.read()
 
         # Detect the faces
@@ -50,14 +65,15 @@ def main():
             print(cx, cy)
             
             # Control the inputs based on the position of the face
-            if (cx < -x_threshold):
-                keyboard.press(key_x_below_threshold)
-            elif (cx > x_threshold):
-                keyboard.press(key_x_above_threshold)
-            if (cy < -y_threshold):
-                keyboard.press(key_y_below_threshold)
-            elif (cy > y_threshold):
-                keyboard.press(key_y_above_threshold)
+            if (wait_for_inputs == 0):
+                if (cx < -x_threshold):
+                    keyboard.press(key_x_below_threshold)
+                elif (cx > x_threshold):
+                    keyboard.press(key_x_above_threshold)
+                if (cy < -y_threshold):
+                    keyboard.press(key_y_below_threshold)
+                elif (cy > y_threshold):
+                    keyboard.press(key_y_above_threshold)
 
             # Draw a rectangle at the face
             rect_color = (255,0,0)
@@ -66,9 +82,12 @@ def main():
             end_y = y + h
             cv2.rectangle(frame, (x,y), (end_x,end_y), rect_color, stroke)
         
+        if (wait_for_inputs > 0):
+            wait_for_inputs -=1
+
         # The web cam frame
         cv2.imshow('WebCam',frame)
-        if cv2.waitKey(20) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     # When done, exit cleanly
